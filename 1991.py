196268bcf03e3c386b681aed3aaff51d4edf0d73
elimerins <elimerins@gmail.com>
2020-01-11 21:00:05 +0900

    modify 1991

--- 1991.py
+++ 1991.py
@@ -4,34 +4,48 @@ node_graph={i:[] for i in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"}
 for _ in range(node):
     node, left, right = map(str,input().split())
     node_graph[node] += [left,right]
+
 #preorder
 def preorder(node_graph, root):
     stack = [root]
     visit = ''
     while stack:
         node = stack.pop()
-        if node not in visit and node != '.':
-            visit += node
+        visit += node
+        if node_graph[node][1] != '.':
             stack.append(node_graph[node][1])
+        if node_graph[node][0] != '.':
             stack.append(node_graph[node][0])
 
     return visit
 #inorder
 def inorder(node_graph, root):
-    stack=[]
+    stack=[root]
     visit = ''
-    node=root
-    while(True):
-        while(node != "."):
-            stack.append(node)
-            node = node_graph[node][0]
-        if stack:
-            node = stack.pop()
-            visit += node
-            node = node_graph[node][1]
+    while stack:
+        if node_graph[stack[-1]][0] != '.' and node_graph[stack[-1]][0] not in visit:
+            stack.append(node_graph[stack[-1]][0])
         else:
-            break
+            node=stack.pop()
+            visit += node
+            if node_graph[node][1] != '.':
+                stack.append(node_graph[node][1])
+
+    return visit
+#postorder
+def postorder(node_graph, root):
+    stack = [root]
+    visit = ''
+    while stack:
+        if node_graph[stack[-1]][0] != '.' and node_graph[stack[-1]][0] not in visit:
+            stack.append(node_graph[stack[-1]][0])
+        elif node_graph[stack[-1]][1] == '.' or node_graph[stack[-1]][1] in visit:
+            visit += stack.pop()#root node append
+        else:
+            stack.append(node_graph[stack[-1]][1])
     return visit
 
+
 print(preorder(node_graph,"A"))
-#print(inorder(node_graph,"A"))
+print(inorder(node_graph,"A"))
+print(postorder(node_graph,'A'))
